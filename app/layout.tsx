b96b34dc0783fc5684fa298ac824fced41d1ef2d
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-04 18:21:16 -0300

    feat: fix problems and create other components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: "Airbnb",
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="Submit" title="Hello" isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
